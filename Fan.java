e12a5ded1f13322d39d49e4d4ae4dd0076f599e1
laura-williams-1 <laurawilliams@pursuit.org>
2025-05-19 15:42:23 -0400

    Creation of instance fields, static field and constructours for Fan class

---
+++ Fan.java
@@ -0,0 +1,18 @@
+public class Fan {
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+    static int FAST;
+    static int MEDIUM;
+    static int SLOW;
+    Fan(int speed, boolean on, double radius, String color){
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+    Fan(){
+        this(0,false,0.00,"Unknown");
+    }
+}
