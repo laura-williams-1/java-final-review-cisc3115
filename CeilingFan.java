1ab04e54193ba71a18499984f5dfb7b7e4b382d4
laura-williams-1 <laurawilliams@pursuit.org>
2025-05-19 15:35:27 -0400

    creation on CeilingFan constructor with inherited instance fields from Fan

--- CeilingFan.java
+++ CeilingFan.java
@@ -1,2 +1,7 @@
 public class CeilingFan extends Fan{
+    private int numBulbs;
+    CeilingFan(int speed, boolean on, double radius, String color, int numBulbs){
+        super(speed, on, raidus, color);
+        this.numBulbs = numBulbs;
+    }
 }
