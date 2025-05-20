4d72ad0325ddfd715f5a33d2c97a700156552573
laura-williams-1 <laurawilliams@pursuit.org>
2025-05-20 18:37:27 -0400

    part 3/ overriding equals method of ceiling fan

--- CeilingFan.java
+++ CeilingFan.java
@@ -4,4 +4,13 @@ public class CeilingFan extends Fan{
         super(speed, on, raidus, color);
         this.numBulbs = numBulbs;
     }
+
+    @Override
+    public boolean equals(CeilingFan c){
+        if (c == null) return false;
+        if (c.getClass() != this.getClass()) return false;
+        CeilingFan cf = CeilingFan(c); // casting done
+        return super.equals(cf) && c.numBulbs == this.numBulbs; // using super equals method and comparing new private instance
+        // super.equals already compares the instance fields from fan to the other ceiling fan
+    }
 }
