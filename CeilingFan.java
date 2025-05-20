e15e5f9f911016097559586b873122f3fdb5fd31
laura-williams-1 <laurawilliams@pursuit.org>
2025-05-20 18:41:40 -0400

    no param constructor

--- CeilingFan.java
+++ CeilingFan.java
@@ -1,8 +1,13 @@
 public class CeilingFan extends Fan{
     private int numBulbs;
     CeilingFan(int speed, boolean on, double radius, String color, int numBulbs){
-        super(speed, on, raidus, color);
+        super(speed, on, radius, color);
         this.numBulbs = numBulbs;
+    }
+
+    CeilingFan(){
+        super(0,false,0.00,"Unknown");
+        this.numBulbs = 0;
     }
 
     @Override
@@ -13,4 +18,6 @@ public class CeilingFan extends Fan{
         return super.equals(cf) && c.numBulbs == this.numBulbs; // using super equals method and comparing new private instance
         // super.equals already compares the instance fields from fan to the other ceiling fan
     }
+
+
 }
